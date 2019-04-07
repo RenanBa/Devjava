69e7e5113c9d6a090cba561aece1a342f6ad91da
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-07 16:59:24 -0700

    Print Circumference and Area of a Circle

--- Matematica/Exercicios/ChapterTwo/DiameterCircumferenceAreaOfCircle.java
+++ Matematica/Exercicios/ChapterTwo/DiameterCircumferenceAreaOfCircle.java
@@ -11,14 +11,19 @@ public class DiameterCircumferenceAreaOfCircle
     {
       // r = radius; diameter = 2r; PI = value of PI; circumference = 2PIr
 
-      float r; // store users radius
-
+      int r; // store users radus
       Scanner input = new Scanner( System.in );
 
       System.out.println( "Enter a radius value: " ); // prompt
       r = input.nextInt(); // store radius
 
+      // calculate and print out the diameter
+      System.out.printf( "The Diameter of %d * %f is %f\n", 2, r, ( 2 * r ) );
 
-      System.out.printf( "the diameter of %d * %f is %f\n", 2, r, ( 2 * r ) );
+      // calculate and print out the circumference
+      System.out.printf( "The Circumference of %d * %f * %f is %f\n", 2, Math.PI , r, ( 2 * Math.PI * r ) );
+
+      // calculate and print out the area of a circle
+      System.out.printf( "The Area of a Circle of %f * %f is %f\n", Math.PI, r, ( Math.PI * ( r * r ) ) );
     }
   }
