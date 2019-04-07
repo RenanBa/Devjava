31c463d5612be45486717504d3732ef12bbe45f6
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-07 16:08:31 -0700

    Create classe and with description

---
+++ Matematica/Exercicios/ChapterTwo/DiameterCircumferenceAreaOfCircle.java
@@ -0,0 +1,18 @@
+// Write an application that inputs from the user the radius of a circle as an
+// integer and prints the circle's diameter, circumference and area using the
+// flating-point value 3.14159 for PI. Use Math.PI for the value of PI and
+// the format specifier %f in a System.out.printf
+
+import java.util.Scanner; // import class Scanner
+
+public class DiameterCircumferenceAreaOfCircle
+  {
+    public static void main ( String[] args )
+    {
+      // r = radius; diameter = 2r; PI = value of PI; circumference = 2PIr
+
+
+
+      System.out.println( System.in ).nextInt();
+    }
+  }
