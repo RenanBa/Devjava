007bcbc94c478d208d0f6ea55d32cb60674bdcea
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-07 09:52:02 -0700

    Create arithimetc file and class with user prompt

---
+++ Matematica/Exercicios/ChapterTwo/Arithmetic.java
@@ -0,0 +1,24 @@
+/* Write an application that that ask the user to enter two integers, obtains
+them from the user and prints their sum, produc, difference and quotient(division). */
+
+import java.util.Scanner; // uses Scanner class to get user input
+
+public class Arithmetic
+  {
+      // main method begins the execution of Java application
+      public static void main ( String[] args )
+      {
+        // cerate Scanner to obtain user input
+        Scanner input = new Scanner ( System.in );
+
+        int number1; // first number to add
+        int number2; // second number to add
+
+        System.out.print( "Enter the first integer: ");
+        number1 = input.nextInt(); // read first number from user
+
+        System.out.print( "Enter the second integer: ");
+        number2 = input.nextInt(); // read second number from user
+
+      }
+  }
