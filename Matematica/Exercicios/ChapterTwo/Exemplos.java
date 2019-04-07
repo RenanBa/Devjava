85a60815847eafc7af18826f78c97a15e01f3502
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-06 23:05:59 -0700

    Print function

---
+++ Matematica/Exercicios/ChapterTwo/Exemplos.java
@@ -0,0 +1,31 @@
+// respostas das atividades do capitulo 2 do livro
+
+import java.util.Scanner; // import Scanner class
+
+public class Exemplos
+{
+    // method main initiates the Java app execution
+    public static void main ( String[] args )
+    {
+        // create a Scanner to obtain input from command line
+        Scanner input = new Scanner( System.in );
+        int number;
+        int x = 2;
+        int y = 3;
+
+        // System.out.print( "Enter an Integer: " ); // Display a message leaving the cursor on the same line
+        // number = input.nextInt();
+
+        //  System.out.printf( "Number is %d/n", number); // display number
+
+
+        /*
+        // Found out the outputs
+        System.out.printf( "x = %d\n", x); // output: x = 2
+        System.out.printf( "value of %d + %d is %d\n", x, x, ( x + x ) ); // output: value of 2 + 2 is 4
+        System.out.printf( "x =" ); // output: x =
+        System.out.printf( "%d = %d\n", ( x + y ), (y + x ) ); // output: 5 = 5
+        */
+
+    } // method main end
+} // class Exemplos end
