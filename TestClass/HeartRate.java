0d55081783faeb04c753d2d7e0b4632051f6f6a4
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-09 20:13:43 -0700

    Create HeartRate.java

---
+++ TestClass/HeartRate.java
@@ -0,0 +1,11 @@
+// Create a class with the attributes persons first name, last name,
+// and date of birth (consisting of separated attributes for the month,
+// day and year of birth). This class should have a constructor that receives
+// this data as parameters. For each attribute provides set and get methods.
+// The class also should returns the person's maximum heart rate and a method
+// that calculates and returns the person's targer heart rate. Write a Java
+// application that prompts for the person's information, instantiate an object
+// of class HeartRates nad print the information from that object then calculates
+// the person's age in year. maximum heart rate and target-heart-rate range.
+// 220 minus your age in year is the formula for maximum heart rate
+// Targer hear rate is 50% to 85% of your maximum heart rate
