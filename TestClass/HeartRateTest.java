90ffcfc7e1810a195080e30fa3cd4cf9e3af44bc
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-09 21:35:27 -0700

    Create HearRateTest file, added class and instantiate HeartRate class

---
+++ TestClass/HeartRateTest.java
@@ -0,0 +1,13 @@
+// Test class to test HeatRate class
+// firstN, lastN, day, month, year
+
+import java.util.Scanner;
+
+public class HeatRateTest
+{
+  public static void main ( String[] args )
+  {
+    HeartRate person1 = new HeatRate( "Renan", "Souza", 6, 1, 1987 );
+    
+  }
+}
