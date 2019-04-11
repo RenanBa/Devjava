c75f1eec4f83189c1291030e5a737bfc69884d19
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-11 08:40:51 -0700

    Create loop to create diagonal lines

--- GUI/drawing/DrawWithLoop/DrawWithLoop.java
+++ GUI/drawing/DrawWithLoop/DrawWithLoop.java
@@ -13,18 +13,30 @@ public class DrawWithLoop extends JPanel
 
         int width = getWidth(); // total width
         int height = getHeight(); // total height
+        int lines = 15; // total numbers of lines
+        int x = 250;
+        int y = 1;
 
-        // draw a line from the upper-left to the lower-right
-        g.drawLine( 0, 0, width, height );
+        // creates 14 lines in an angle
+        while ( lines > 1 )
+        {
+          g.drawLine( 0, 0, x , y );
 
-        // draw a line from the upper-left to the lower-right
-        g.drawLine( 7, 0, width, height );
+          y = y + 17;
+          x = x - 17;
+          lines -= 1;
+        }
+
 
         // draw a line from the lower-left to the upper-right
-        g.drawLine( 0, height, width, 0 );
-
-        // draw a line from the lower-left to the upper-right
-        g.drawLine( 7, height, width, 0 );
+        // g.drawLine( 0, height, width, 0 );
 
     } // end method paintComponent
 } // end class DrawWithLoop
+
+
+
+// // draw a line from the upper-left to the lower-right
+// g.drawLine( 0, 0, ( width ), ( height / y ) ); // horizontal line
+// g.drawLine( 0, 0, ( width / 2 ), ( height / 2 ) ); // diagonal line (half)
+// g.drawLine( 0, 0, ( width / x ), ( height ) ); // vertival line
