f12d8888819ce51c97dd6c8aebbee96671002e16
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-11 06:01:26 -0700

    Add basic structure from last DrawPanel exercise

---
+++ GUI/drawing/DrawWithLoop.java
@@ -0,0 +1,23 @@
+// Draw a Panel using loop using 15 lines from left-top to left bottom and going to right-top accordingly
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class DrawWithLoop extends JPanel
+{
+    // draws an X from the corners of the panel
+    public void paintComponent( Graphics g )
+    {
+        // call paintComponent to ensure the panel displaus correctly
+        super.paintComponent( g );
+
+        int width = getWidth(); // total width
+        int height = getHeight(); // total height
+
+        // draw a line from the upper-left to the lower-right
+        g.drawLine( 0, 0, width, height );
+
+        // draw a line from the lower-left to the upper-right
+        g.drawLine( 0, height, width, 0 );
+    } // end method paintComponent
+} // end class DrawPanel
---
+++ GUI/drawing/DrawWithLoopTest.java
@@ -0,0 +1,21 @@
+// Application to display a DrawWithLoop
+import javax.swing.JFrame;
+
+public class DrawWithLoopTest
+{
+    public static void main( String[] args )
+    {
+        // create a panel that contains our drawing
+        DrawPanel panel = new DrawPanel();
+
+        // create a frame to hold the panel
+        JFrame application = new JFrame();
+
+        // set the frame to exit when it is closed
+        application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        application.add( panel ); // add the panel to the frame
+        application.setSize( 250, 250 ); // set the size of the frame
+        application.setVisible( true ); // make the frame visible
+    } // end main
+} // end DrawPanelTest
