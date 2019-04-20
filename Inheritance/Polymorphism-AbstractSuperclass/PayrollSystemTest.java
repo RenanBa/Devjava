6ddf62a22d23ff0ff1a941f7d8957a9ed15b0ddd
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-19 23:09:00 -0700

    Fix classes name title

--- Inheritance/Polymorphism-AbstractSuperclass/PayrollSystemTest.java
+++ Inheritance/Polymorphism-AbstractSuperclass/PayrollSystemTest.java
@@ -5,17 +5,17 @@ public class PayrollSystemTest
     public static void main ( String[] args )
     {
         // create subclass objectes
-        SalariedEmployee salariedEmployee = new SalariedEmployee( "John", "Smith", "111-11-1111", 800,00 );
-        HourlyEployee hourlyEmployee = new HourlyEployee( "Karen", "Price", "222-22-2222", 16.75, 40 );
+        SalariedEmployee salariedEmployee = new SalariedEmployee( "John", "Smith", "111-11-1111", 800.00 );
+        HourlyEmployee hourlyEmployee = new HourlyEmployee( "Karen", "Price", "222-22-2222", 16.75, 40 );
         CommissionEmployee commissionEmployee = new CommissionEmployee( "Sue", "Jones", "333-33-3333", 10000, .06 );
-        BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEployee( "Bob", "Lewis", "444-44-4444", 5000, .04, 300 );
+        BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee( "Bob", "Lewis", "444-44-4444", 5000, .04, 300 );
 
         System.out.println( "Employee processed indicidually:\n" );
 
         System.out.printf( "%s\n%s: $%,.2f\n\n", salariedEmployee, "earned", salariedEmployee.earnings() );
         System.out.printf( "%s\n%s: $%,.2f\n\n", hourlyEmployee, "earned", hourlyEmployee.earnings() );
         System.out.printf( "%s\n%s: $%,.2f\n\n", comissionEmployee, "earned", comissionEmployee.earnings() );
-        System.out.printf( "%s\n%s: $%,.2f\n\n", BasePlusComissionEmployee, "earned", BasePlusComissionEmployee.earnings() );
+        System.out.printf( "%s\n%s: $%,.2f\n\n", basePlusCommissionEmployee, "earned", basePlusCommissionEmployee.earnings() );
 
         // create four-element Employee array
         Employee[] employees = new Employee[ 4 ];
