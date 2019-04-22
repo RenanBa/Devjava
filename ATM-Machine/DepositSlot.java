f9c3bd8fc1aea1c08aeaf19ad0343b1a227b5c34
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-21 22:40:03 -0700

    Create and implement DepositSlot class

---
+++ ATM-Machine/DepositSlot.java
@@ -0,0 +1,11 @@
+// Represent the deposit slot of the ATM
+
+public class DepositSlot
+{
+    // indicates whether envelope was received (always return true, because
+    // this is only a software simulation of a real deposit slot)
+    public boolean isEnvelopeReceived()
+    {
+        return true; // depoisit envelop was received
+    } // end method isEnvelopeReceived
+} // end class DepositSlot
