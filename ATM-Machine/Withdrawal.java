e03585f483766fa2bc987f22dc71ae2d5159b502
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-21 21:30:47 -0700

    Implement Withdrawal class

--- ATM-Machine/Withdrawal.java
+++ ATM-Machine/Withdrawal.java
@@ -2,5 +2,19 @@
 
 public class Withdrawal extends Transaction
 {
+    // attributes
+    private double amount; // amount to withdraw
+    private Keypad keypad; // reference to keypad
+    private CashDispenser cashDispenser; // reference to cash dispenser
 
+    // no argument constructor
+    public Withdrawal()
+    {
+    } // end no argument Withdrawal constructor
+
+    // method overriding execute
+    @Override
+    public void execute()
+    {
+    } // end method execute
 } // end class
