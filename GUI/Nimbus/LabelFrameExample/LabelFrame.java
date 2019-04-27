57999f0aa44ab65727ee1c357a056e21a20af2a4
RenanBa <renanbarbosadesouza@gmail.com>
2019-04-27 15:32:02 -0700

    Compile and run files

--- GUI/Nimbus/LabelFrameExample/LabelFrame.java
+++ GUI/Nimbus/LabelFrameExample/LabelFrame.java
@@ -16,23 +16,26 @@ public class LabelFrame extends JFrame
     // LabelFrame constructor adds JLables to JFrame
     public LabelFrame()
     {
-        super( "Testing JLable1" ); // set frame layout
+        super( "Testing JLable1" );
+        setLayout( new FlowLayout() ); // set frame layout
 
         //JLable constructor with a string argument
         label1 = new JLabel( "Label with text" );
         label1.setToolTipText( "This is label1" );
-        add( lebel1 ); // added label1 to JFrame
+        add( label1 ); // added label1 to JFrame
 
         // JLabel constructor with string, Icon and Alignment arguments
-        Icon bug = new ImageIcon( getClass().getResource( "bug1.png" ) ) ;
-        label2 = new JLabel( "Label with test and icon", bug, SwingCoonstants.LEFT );
+        Icon bug = new ImageIcon( getClass().getResource( "v.png" ) ) ;
+        label2 = new JLabel( "Label with test and icon", bug, SwingConstants.LEFT );
         label2.setToolTipText( "This is label2" );
         add( label2 ); // added label2 to JFrame
 
         label3 = new JLabel(); // JLabel constructor no argument
         label3.setText( "Label with icon and text at bottom" );
+        label3.setIcon( bug ); // add icon to JLabel
         label3.setHorizontalTextPosition( SwingConstants.CENTER );
-        lavel3.setToolTipText( "This is label3" );
+        label3.setVerticalTextPosition( SwingConstants.BOTTOM );
+        label3.setToolTipText( "This is label3" );
         add( label3 ); // added label3 to JFrame
     } // end LabelFrame constructor
 } // end class LabelFrame
